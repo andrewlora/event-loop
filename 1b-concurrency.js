20f60f5830c36b45bed05749131b058af8b58a41
Andres Lora <jalc.andres.lora@gmail.com>
2024-01-20 20:13:26 -0700

    added promises and callbacks event loop phases

--- 1b-concurrency.js
+++ 1b-concurrency.js
@@ -1,5 +1,7 @@
 // UV_THREADPOOL_SIZE=4 node 1b-concurrency.js
 // UV_THREADPOOL_SIZE is the size of threads into EventLoop. By default is 4 and the main thread in NodeJS
+// UV_ represent the LIBUV
+// NodeJS has a main single thread, but with eventLoop will be 4, 5 in total by default
 
 const crypto = require("node:crypto");
 const rounds = 50000;
---
+++ 4-promises-callbacks.js
@@ -0,0 +1,15 @@
+setImmediate(() => {
+  console.log("setImmediate");
+});
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise");
+});
+
+process.nextTick(() => {
+  console.log("process.nextTick");
+});
